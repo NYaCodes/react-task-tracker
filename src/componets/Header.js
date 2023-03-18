a6ea6cb24c04654dd83390d7169714e3ced6f602
NYaCodes <107141629+NYaCodes@users.noreply.github.com>
2023-03-17 22:54:53 -0500

    Updating Button.js & Header.js & created file Tasks.js

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -1,8 +1,13 @@
+import { click } from "@testing-library/user-event/dist/click"
 import  PropTypes from "prop-types"
 import Button from "./Button"
 
 const Header = ({title}) => {
-    return (
+    const onClick = () =>{
+        console.log(`Click`)
+    }
+    
+    return ( 
         <header className="header">
 <h1>{title}</h1>
 <Button color='green' text='Hello' />
